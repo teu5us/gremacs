d7833db5d64eb86d5d5a6a48642272271f08c55b
Pavel Stepanov <paulkreuzmann@gmail.com>
2021-10-12 22:36:25 +0600

    update outline popup definition

--- m/pdf.el
+++ m/pdf.el
@@ -10,7 +10,7 @@
   (pdf-view-continuous t)
   (pdf-view-display-size 'fit-height)
   :config
-  (p/defpopup "*Outline.*" :side 'right :width 0.3)
+  (p/defpopup "*Outline.*" t :side 'right :width 0.3)
   (:maps (:n) pdf-view-mode-map "SPC" #'evil-send-leader
          (:n) pdf-view-mode-map "M-SPC" #'evil-send-localleader))
 
