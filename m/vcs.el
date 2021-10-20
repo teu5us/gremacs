ee249a9e5fe0ef149c6accfd1decf5c7dd05a9a5
Pavel Stepanov <paulkreuzmann@gmail.com>
2021-10-21 00:23:43 +0600

    kill magit buffers after killing magit-status buffer

--- m/vcs.el
+++ m/vcs.el
@@ -4,4 +4,12 @@
   ;; :commands (magit-status)
   :defer 1
   :config
+  (defun p/magit-kill-buffers ()
+    "Restore window configuration and kill all Magit buffers."
+    (interactive)
+    (let ((buffers (magit-mode-get-buffers)))
+      (magit-restore-window-configuration)
+      (mapc #'kill-buffer buffers)))
+
+  (evil-define-key '(normal visual) magit-status-mode-map "q" #'p/magit-kill-buffers)
   (:maps :a global "<leader>gg" #'magit-status))
