9ad1c4068a3f3ebfea0c72d6fe98c011bc3e1b91
Pavel Stepanov <paulkreuzmann@gmail.com>
2021-11-17 19:26:22 +0600

    hook additional haskell modes

--- lang/haskell.el
+++ lang/haskell.el
@@ -2,4 +2,9 @@
 
 (use-package haskell-mode
   :mode "\\.hs\\'"
+  :hook
+  (haskell-mode . interactive-haskell-mode)
+  (haskell-mode . haskell-indentation-mode)
+  (haskell-mode . haskell-decl-scan-mode)
+  (haskell-mode . haskell-doc-mode)
   :bind (:map haskell-mode-map ("C-c C-z" . haskell-interactive-bring)))
