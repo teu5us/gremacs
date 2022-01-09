688f5710b42ab7d80e63ec702c62123b60fcae6a
Pavel Stepanov <pavelstepanov@vivaldi.net>
2022-01-09 21:11:17 +0600

    fix magit loading

--- m/vcs.el
+++ m/vcs.el
@@ -1,8 +1,9 @@
 ;;; vcs.el --- Git configuration -*- lexical-binding: t; -*-
 
 (use-package magit
-  ;; :commands (magit-status)
-  :defer 1
+  :commands (magit-status)
+  :init
+  (:maps :a global "<leader>gg" #'magit-status)
   :config
   (defun p/magit-kill-buffers ()
     "Restore window configuration and kill all Magit buffers."
@@ -11,5 +12,4 @@
       (magit-restore-window-configuration)
       (mapc #'kill-buffer buffers)))
 
-  (:map (:n :v) magit-status-mode-map "q" #'p/magit-kill-buffers)
-  (:maps :a global "<leader>gg" #'magit-status))
+  (:map (:n :v) magit-status-mode-map "q" #'p/magit-kill-buffers))
