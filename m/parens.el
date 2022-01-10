6c6361b8ffbcd7e1cb6ea9b5b599ab0f3c7fe7bf
Pavel Stepanov <pavelstepanov@vivaldi.net>
2022-01-10 23:22:19 +0600

    only leave hydra for now, maybe add evil-lisp-state

--- m/parens.el
+++ m/parens.el
@@ -23,39 +23,39 @@
   :config
   (p/require 'smartparens 'smartparens-config)
 ;;;;;; define motions for lispy modes
-  (defun p/load-lispy-parens-bindings ()
-    (:maps (:n :i) local "M-<backspace>" #'sp-backward-kill-sexp
-           (:n :i) local "M-D" #'sp-kill-sexp
-           (:i) local "<return>" #'sp-newline
-           (:n :i) local "M-c" #'sp-clone-sexp
-           (:n :i) local "M-C" #'sp-copy-sexp
-           (:n :i) local "M-r" #'sp-raise-sexp
-           (:n :i) local "M-e" #'sp-emit-sexp
-           (:n :i) local "M-S" #'sp-split-sexp
-           (:n :i) local "M-j" #'sp-join-sexp
-           (:n :i) local "M-a" #'sp-absorb-sexp
-           (:n :i) local "M-t" #'sp-transpose-sexp
-           (:n :i) local "M-;" #'sp-comment
-           (:n :i) local "M-I" #'sp-change-inner
-           (:n :i) local "M-E" #'sp-change-enclosing
-           (:n :i) local "C-M-N" #'sp-change-enclosing
-           (:n :v :i) local "M-o" #'sp-beginning-of-sexp
-           (:n :v :i) local "M-O" #'sp-end-of-sexp
-           (:n :v :i) local "M-w" #'sp-beginning-of-next-sexp
-           (:n :v :i) local "M-W" #'sp-beginning-of-previous-sexp
-           (:n :v :i) local "M-f" #'sp-forward-symbol
-           (:n :v :i) local "M-b" #'sp-backward-symbol
-           (:n :v :i) local "M-n" #'sp-forward-sexp
-           (:n :v :i) local "M-p" #'sp-backward-sexp
-           (:n :v :i) local "M-H" #'sp-down-sexp
-           (:n :v :i) local "M-h" #'sp-backward-down-sexp
-           (:n :v :i) local "M-l" #'sp-up-sexp
-           (:n :v :i) local "M-L" #'sp-backward-up-sexp
-           (:n :v :i) local "M-]" #'sp-forward-slurp-sexp
-           (:n :v :i) local "M-}" #'sp-forward-barf-sexp
-           (:n :v :i) local "M-[" #'sp-backward-slurp-sexp
-           (:n :v :i) local "M-{" #'sp-backward-barf-sexp
-           (:n :v :i) local "M-/" #'sp-splice-sexp))
+  ;; (defun p/load-lispy-parens-bindings ()
+  ;;   (:maps (:n :i) local "M-<backspace>" #'sp-backward-kill-sexp
+  ;;          (:n :i) local "M-D" #'sp-kill-sexp
+  ;;          (:i) local "<return>" #'sp-newline
+  ;;          (:n :i) local "M-c" #'sp-clone-sexp
+  ;;          (:n :i) local "M-C" #'sp-copy-sexp
+  ;;          (:n :i) local "M-r" #'sp-raise-sexp
+  ;;          (:n :i) local "M-e" #'sp-emit-sexp
+  ;;          (:n :i) local "M-S" #'sp-split-sexp
+  ;;          (:n :i) local "M-j" #'sp-join-sexp
+  ;;          (:n :i) local "M-a" #'sp-absorb-sexp
+  ;;          (:n :i) local "M-t" #'sp-transpose-sexp
+  ;;          (:n :i) local "M-;" #'sp-comment
+  ;;          (:n :i) local "M-I" #'sp-change-inner
+  ;;          (:n :i) local "M-E" #'sp-change-enclosing
+  ;;          (:n :i) local "C-M-N" #'sp-change-enclosing
+  ;;          (:n :v :i) local "M-o" #'sp-beginning-of-sexp
+  ;;          (:n :v :i) local "M-O" #'sp-end-of-sexp
+  ;;          (:n :v :i) local "M-w" #'sp-beginning-of-next-sexp
+  ;;          (:n :v :i) local "M-W" #'sp-beginning-of-previous-sexp
+  ;;          (:n :v :i) local "M-f" #'sp-forward-symbol
+  ;;          (:n :v :i) local "M-b" #'sp-backward-symbol
+  ;;          (:n :v :i) local "M-n" #'sp-forward-sexp
+  ;;          (:n :v :i) local "M-p" #'sp-backward-sexp
+  ;;          (:n :v :i) local "M-H" #'sp-down-sexp
+  ;;          (:n :v :i) local "M-h" #'sp-backward-down-sexp
+  ;;          (:n :v :i) local "M-l" #'sp-up-sexp
+  ;;          (:n :v :i) local "M-L" #'sp-backward-up-sexp
+  ;;          (:n :v :i) local "M-]" #'sp-forward-slurp-sexp
+  ;;          (:n :v :i) local "M-}" #'sp-forward-barf-sexp
+  ;;          (:n :v :i) local "M-[" #'sp-backward-slurp-sexp
+  ;;          (:n :v :i) local "M-{" #'sp-backward-barf-sexp
+  ;;          (:n :v :i) local "M-/" #'sp-splice-sexp))
 ;;;;;; define a hydra for lispy keys
   (defhydra hydra-sp (:pre (hydra-set-property 'hydra-sp :verbosity 0)
                            :color red
@@ -82,8 +82,8 @@
     ("I" sp-down-sexp "up sexp")
     ("i" sp-backward-down-sexp "backward down sexp")
 
-    ("ci" sp-change-inner "change inner" :column "Actions")
-    ("ce" sp-change-enclosing "change enclosing")
+    ("ci" sp-change-inner "change inner" :exit t :column "Actions")
+    ("ce" sp-change-enclosing "change enclosing" :exit t)
     ("xa" sp-absorb-sexp "absorb")
     ("xc" sp-convolute-sexp "convolute")
     ("xe" sp-emit-sexp "emit")
@@ -115,7 +115,7 @@
     ("cC" sp-backward-copy-sexp "copy backwards")
     ("u" undo "undo")
 
-    ("?" (hydra-set-property 'hydra-sp :verbosity 1) "hide" :column "Etc")
+    ("?" (hydra-set-property 'hydra-sp :verbosity 1) "help" :column "Etc")
     (":" execute-extended-command "EX")
     ("q" nil "quit" :color blue)
     ("<escape>" nil "quit" :color blue))
@@ -129,6 +129,7 @@
 ;;;;; hooks
   :hook
   ((text-mode org-mode) . smartparens-mode)
+  ;; (prog-mode . smartparens-strict-mode)
   ((sly-mrepl-mode prog-mode)
    .
    (lambda ()
@@ -136,7 +137,7 @@
      (if (p/lisp?)
          (progn
            (local-set-key (kbd "C-l") #'hydra-sp/body)
-           (p/load-lispy-parens-bindings)
+           ;; (p/load-lispy-parens-bindings)
            (setq-local evil-move-beyond-eol t))
        (:maps (:n :v :i) local "M-w" #'sp-slurp-hybrid-sexp)))))
 
@@ -147,3 +148,9 @@
   :hook ((smartparens-mode smartparens-strict-mode)
          .
          evil-smartparens-mode))
+
+;;;; load evil-lisp-state
+;; (use-package evil-lisp-state
+;;   :after (evil smartparens)
+;;   :custom (evil-lisp-state-global t)
+;;   :config (evil-lisp-state-leader "C-l"))
