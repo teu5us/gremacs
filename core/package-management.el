cbae50bb7cd99cab565a891396bf7b6d6931047c
Pavel Stepanov <paulkreuzmann@gmail.com>
2022-01-13 11:50:26 +0600

    don't watch for changes

--- core/package-management.el
+++ core/package-management.el
@@ -4,10 +4,11 @@
 (defvar bootstrap-version)
 
 (defvar straight-build-cache-fixed-name (format "build-%s-cache.el" emacs-version))
-(defvar straight-check-for-modifications
-  (if (executable-find "watchexec")
-      '(watch-files find-when-checking)
-    nil))
+;; (defvar straight-check-for-modifications
+;;   (if (executable-find "watchexec")
+;;       '(watch-files find-when-checking)
+;;     nil))
+(defvar straight-check-for-modifications nil)
 (defvar straight-use-package-by-default t)
 (defvar straight-repository-branch "develop")
 (defvar straight-build-dir (format "build-%s" emacs-version))
