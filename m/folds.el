3ac701329196546e22ec90077fa616f04aa10a93
Pavel Stepanov <paulkreuzmann@gmail.com>
2022-02-28 11:25:27 +0600

    add origami code folding

---
+++ m/folds.el
@@ -0,0 +1,12 @@
+;;; folds.el --- code folding using origami -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; Set up code folding.
+
+;;; Code:
+
+(use-package origami
+  :hook (prog-mode . origami-mode))
+
+;;; folds.el ends here
