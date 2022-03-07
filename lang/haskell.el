ce60c612df9a26597dd9254f56b96010c0b5b9aa
Pavel Stepanov <paulkreuzmann@gmail.com>
2022-03-07 09:37:12 +0600

    load doc-mode

--- lang/haskell.el
+++ lang/haskell.el
@@ -5,9 +5,18 @@
   :hook
   (haskell-mode . interactive-haskell-mode)
   (haskell-mode . haskell-indentation-mode)
-  (haskell-mode . haskell-decl-scan-mode)
-  (haskell-mode . haskell-doc-mode)
-  :bind (:map haskell-mode-map ("C-c C-z" . haskell-interactive-bring)))
+  :bind (:map haskell-mode-map ("C-c C-z" . haskell-interactive-bring))
+  :config
+  (ignore-errors
+    (require 'haskell-doc)
+    (require 'haskell-decl-scan))
+  (when (find-library-name "haskell-doc")
+    (setq-default haskell-doc-show-global-types t
+                  haskell-doc-show-prelude t
+                  haskell-doc-show-reserved t
+                  haskell-doc-show-strategy t
+                  haskell-doc-show-user-defined t)
+    (add-hook 'haskell-mode-hook #'haskell-doc-mode)))
 
 (use-package lsp-haskell
   :hook (haskell-mode . lsp))
