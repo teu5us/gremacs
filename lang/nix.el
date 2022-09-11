be875ae4a83f6d2259310dfdb2d51a6cbcadfba7
Pavel Stepanov <paulkreuzmann@gmail.com>
2022-09-12 00:52:28 +0600

    indent line in one tab press

--- lang/nix.el
+++ lang/nix.el
@@ -1,7 +1,9 @@
 ;;; nix.el --- nix setup -*- lexical-binding: t; -*-
 
 (use-package nix-mode
-  :mode "\\.nix\\'")
+  :mode "\\.nix\\'"
+  :config
+  (:maps (:i) nix-mode-map "<tab>" #'nix-indent-line))
 
 (use-package envrc
   :commands (envrc-mode))
