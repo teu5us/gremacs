029b10915d65716c97cb38f932b0d275f477bcf6
Pavel Stepanov <paulkreuzmann@gmail.com>
2022-09-12 00:52:16 +0600

    change indentation defaults

--- core/indentation.el
+++ core/indentation.el
@@ -1,7 +1,7 @@
 ;; indentation.el --- Configure indentation -*- lexical-binding: t; -*-
 
 (defun p/setup-indentation ()
-  (setq-default electric-indent-inhibit t)
+  ;; (setq-default electric-indent-inhibit t)
   (setq-default backward-delete-char-untabify-method 'hungry)
   (setq-default indent-tabs-mode nil)
   (setq-default indent-line-function 'insert-tab)
@@ -13,13 +13,14 @@
   (c-set-offset 'case-label '+)
   (c-set-offset 'access-label 0)
   (c-set-offset (quote cpp-macro) 0 nil)
-  (defun smart-electric-indent-mode ()
-    "Disable 'electric-indent-mode in certain buffers and enable otherwise."
-    (cond ((and (eq electric-indent-mode t)
-                (member major-mode '(erc-mode text-mode)))
-           (electric-indent-mode 0))
-          ((eq electric-indent-mode nil) (electric-indent-mode 1))))
-  (add-hook 'post-command-hook #'smart-electric-indent-mode))
+  ;; (defun smart-electric-indent-mode ()
+  ;;   "Disable 'electric-indent-mode in certain buffers and enable otherwise."
+  ;;   (cond ((and (eq electric-indent-mode t)
+  ;;               (member major-mode '(erc-mode text-mode)))
+  ;;          (electric-indent-mode 0))
+  ;;         ((eq electric-indent-mode nil) (electric-indent-mode 1))))
+  ;; (add-hook 'post-command-hook #'smart-electric-indent-mode))
+  (add-hook 'prog-mode-hook #'electric-indent-local-mode))
 
 (add-hook 'after-init-hook #'p/setup-indentation)
 
