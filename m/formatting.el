2173a7acf8ac958a2cd671fb1d847e6118da8487
Pavel Stepanov <paulkreuzmann@gmail.com>
2022-09-16 17:05:04 +0600

    format code on save

--- m/formatting.el
+++ m/formatting.el
@@ -1,4 +1,6 @@
 ;;; formatting.el --- Format-all -*- lexical-binding: t; -*-
 
 (use-package format-all
+  :demand t
+  :hook (prog-mode . format-all-buffer)
   :bind ("<leader>cf" . format-all-buffer))
